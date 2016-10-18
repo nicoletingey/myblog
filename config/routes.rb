5d634f70a665edecc8777f412ed84b19cba1cb30
Nicole Tingey <nicole.tingey1@gmail.com>
2016-10-18 11:47:06 -0600

    Switched database to Postgres

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,10 @@ Rails.application.routes.draw do
 
   get 'pages/Suscribe'
 
+
   resources :posts
+
+  root 'posts#index'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
